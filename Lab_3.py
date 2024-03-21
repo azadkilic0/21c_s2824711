c3aa6c3931e688f9cda25256f78a8e62bb92b910
azadkilic0 <a.kilic1213@gmail.com>
2024-03-21 04:21:30 +0100

    Task-6 Completed

--- Lab_3.py
+++ Lab_3.py
@@ -1,14 +1,15 @@
 import math
+
 #Task-1
 squares = [x**2 for x in range(1, 11)]
-print( squares)
+print( "Task 1 :",squares)
 
 #Task-2
 def generate_squares(start, end):
     return [x**2 for x in range(start, end+1)]
 # Example usage of the function
 example_squares = generate_squares(1, 10)
-print(example_squares)
+print("Task 2 :",example_squares)
 
 #Task-3
 class SquareGenerator:
@@ -19,11 +20,11 @@ class SquareGenerator:
 if __name__ == "__main__":
     square_generator = SquareGenerator()
     squares = square_generator.generate_squares(1, 10)
-    print(squares)
+    print("Task 3 :",squares)
 
 #Task-4
 square_roots = [math.sqrt(number) for number in squares]
-print(square_roots)
+print("Task 4 :",square_roots)
 
 #Task-5
 class SquareGenerator:
@@ -41,7 +42,23 @@ if __name__ == "__main__":
     square_generator = SquareGenerator()
     try:
         squares = square_generator.generate_squares(1, 10)
+        print("Task 5 :")
         print(squares)
         squares_invalid = square_generator.generate_squares(10, 1)  # This should raise an exception
     except ValueError as error:
         print(f"Error: {error}")
+
+#Task-6
+from Lab_3 import SquareGenerator
+if __name__ == "__main__":
+    square_generator = SquareGenerator()
+    try:
+        squares = square_generator.generate_squares(1, 10)
+        print ("Task 6 :")
+        print(squares)
+        # This line is expected to raise an exception due to invalid range
+        squares_invalid = square_generator.generate_squares(10, 1)
+    except ValueError as error:
+        print(f"Error: {error}")
+
+
