76419edcbbca1e11a9e73578207e55038c26dd26
azadkilic0 <a.kilic1213@gmail.com>
2024-03-21 03:44:32 +0100

    Task-2 Completed

--- Lab_3.py
+++ Lab_3.py
@@ -1,3 +1,10 @@
 #Task-1
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+#Task-2
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end+1)]
+# Example usage of the function
+example_squares = generate_squares(1, 10)
+print(example_squares)
