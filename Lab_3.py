63f2439423d3fd361c804ca05affa9745021be9b
azadkilic0 <a.kilic1213@gmail.com>
2024-03-21 03:53:36 +0100

    Task-4 Completed

--- Lab_3.py
+++ Lab_3.py
@@ -1,13 +1,14 @@
+import math
 #Task-1
 squares = [x**2 for x in range(1, 11)]
-print("task-1:"+squares)
+print( squares)
 
 #Task-2
 def generate_squares(start, end):
     return [x**2 for x in range(start, end+1)]
 # Example usage of the function
 example_squares = generate_squares(1, 10)
-print("task-2:"+example_squares)
+print(example_squares)
 
 #Task-3
 class SquareGenerator:
@@ -18,4 +19,8 @@ class SquareGenerator:
 if __name__ == "__main__":
     square_generator = SquareGenerator()
     squares = square_generator.generate_squares(1, 10)
-    print("task-3:"+squares)
+    print(squares)
+
+#Task-4
+square_roots = [math.sqrt(number) for number in squares]
+print(square_roots)
