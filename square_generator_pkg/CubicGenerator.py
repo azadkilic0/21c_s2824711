b99ef03b0ac9166763a2c85d4cec4846f5c7aeec
azadkilic0 <a.kilic1213@gmail.com>
2024-03-21 10:50:18 +0100

    Task-10 Corrected

--- square_generator_pkg/CubicGenerator.py
+++ square_generator_pkg/CubicGenerator.py
@@ -3,18 +3,20 @@ from .SquareGenerator import SquareGenerator
 
 class CubicGenerator(SquareGenerator):
     def generate_squares(self, start, end):
-        """Overrides the abstract method from SquareGenerator to generate cubes instead of squares.
+        """Implements the abstract method from SquareGenerator to generate squares.
+
+        Ensures that the start of the range is less than the end, otherwise, raises a ValueError.
 
         Parameters:
             start (int): The starting number of the range.
             end (int): The ending number of the range.
 
         Returns:
-            list: A list of cubes from start to end.
+            list: A list of squares from start to end.
 
         Raises:
             ValueError: If start is not less than end, indicating the range is invalid.
         """
         if start >= end:
             raise ValueError("Start of range must be less than the end.")
-        return [x ** 3 for x in range(start, end + 1)]
+        return [x ** 2 for x in range(start, end + 1)]
